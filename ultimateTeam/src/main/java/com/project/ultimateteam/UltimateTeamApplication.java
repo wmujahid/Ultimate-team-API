6a6d6e7e8c71cbb47910b706fffe0d0798483bd9
wmujahid <wmujahid47@gmail.com>
2022-02-01 14:05:37 -0500

    created project folder

---
+++ ultimateTeam/src/main/java/com/project/ultimateteam/UltimateTeamApplication.java
@@ -0,0 +1,13 @@
+package com.project.ultimateteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateTeamApplication.class, args);
+    }
+
+}
