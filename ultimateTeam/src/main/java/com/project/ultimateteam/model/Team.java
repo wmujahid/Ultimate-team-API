60fd95e06169454418da92998214851431c9344b
wmujahid <wmujahid47@gmail.com>
2022-02-03 13:02:36 -0500

    connected postgres db and added JPA annotations to teams model

--- ultimateTeam/src/main/java/com/project/ultimateteam/model/Team.java
+++ ultimateTeam/src/main/java/com/project/ultimateteam/model/Team.java
@@ -1,8 +1,20 @@
 package com.project.ultimateteam.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name= "teams")
 public class Team {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String teamName;
+
+    @Column
     private String description;
 
     public Team(Long id, String name, String description) {
