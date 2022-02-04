58a6e6feaa7b1711503da985cadf64a1163520fb
wmujahid <wmujahid47@gmail.com>
2022-02-04 07:49:43 -0500

    added Coach getters/setters and constructors

--- ultimateTeam/src/main/java/com/project/ultimateteam/model/Coach.java
+++ ultimateTeam/src/main/java/com/project/ultimateteam/model/Coach.java
@@ -1,4 +1,87 @@
 package com.project.ultimateteam.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name= "coaches")
 public class Coach {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String nationality;
+
+    @Column
+    private Integer age;
+
+    @Column
+    private String strategy;
+
+    public Coach(Long id, String name, String nationality, Integer age, String strategy) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.age = age;
+        this.strategy = strategy;
+    }
+
+    public Coach() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public String toString() {
+        return "Coach{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", age=" + age +
+                ", strategy='" + strategy + '\'' +
+                '}';
+    }
 }
