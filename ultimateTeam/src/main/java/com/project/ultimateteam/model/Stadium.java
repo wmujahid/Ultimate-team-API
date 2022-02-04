f2cf605855c30fc2011b7429c560bb2ea5d6648c
wmujahid <wmujahid47@gmail.com>
2022-02-04 09:29:04 -0500

    generated getters/setters for stadium model

--- ultimateTeam/src/main/java/com/project/ultimateteam/model/Stadium.java
+++ ultimateTeam/src/main/java/com/project/ultimateteam/model/Stadium.java
@@ -23,4 +23,65 @@ public class Stadium {
     @Column
     private boolean hasChant;
 
+    public Stadium(Long id, String name, String city, Integer seatingCapacity, boolean hasChant) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.seatingCapacity = seatingCapacity;
+        this.hasChant = hasChant;
+    }
+
+    public Stadium() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(Integer seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public boolean isHasChant() {
+        return hasChant;
+    }
+
+    public void setHasChant(boolean hasChant) {
+        this.hasChant = hasChant;
+    }
+
+    @Override
+    public String toString() {
+        return "Stadium{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", seatingCapacity=" + seatingCapacity +
+                ", hasChant=" + hasChant +
+                '}';
+    }
 }
