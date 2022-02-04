619414181dfc31854407bc119b9236d9e6e68c54
wmujahid <wmujahid47@gmail.com>
2022-02-04 09:32:35 -0500

    created StadiumRepository

---
+++ ultimateTeam/src/main/java/com/project/ultimateteam/repository/StadiumRepository.java
@@ -0,0 +1,8 @@
+package com.project.ultimateteam.repository;
+
+import com.project.ultimateteam.model.Stadium;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+
+}
